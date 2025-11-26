83ea50a8fdb4595ab37c0bd0af11ebd8bb5a0567
Eduardo Martínez <e_mtz.m@ciencias.unam.com>
2025-11-26 00:21:33 -0600

    feat: cambios de fuente

--- codeflix/app/layout.tsx
+++ codeflix/app/layout.tsx
@@ -6,6 +6,20 @@ import SideMenu from "@/components/SideMenu";
 import { useState } from "react";
 import "./globals.css";
 
+import { Cinzel } from 'next/font/google';
+const cinzel = Cinzel({ 
+    subsets: ['latin'], 
+    weight: ['700'] 
+});
+
+import { Lora } from 'next/font/google';
+const lora = Lora({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  display: 'swap',
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -16,6 +30,30 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className="bg-[#f6ffff]">
+      <div className="relative w-full h-[30vw] overflow-hidden flex items-center justify-center">
+          {/*video bg */}
+          <video
+            className="absolute inset-0 w-full h-full object-cover"
+            src="/bg.mp4"
+            autoPlay
+            loop
+            muted
+            playsInline
+          />
+          {/*overlay*/}
+          <div className="absolute inset-0 bg-[#004b4b]/70" />
+          <div className="relative text-center pt-[14%] text-white font-bold py-16">
+            <h1 className={`${cinzel.className} text-7xl font-extrabold  mb-6`}>
+              CodeFlix
+            </h1>
+            <p className={`${cinzel.className} text-lg pb-[1%]`}>
+              Tu guía de fantasía
+            </p>
+            <p className={`${lora.className} text-md pb-[1%] font-normal italic`}>
+              Busca, filtra y descubre películas que encajan con tu mundo.
+            </p>
+          </div>
+        </div>
         <Header onMenuClick={() => setMenuOpen(true)} />
         <SideMenu open={menuOpen} onClose={() => setMenuOpen(false)} />
 
